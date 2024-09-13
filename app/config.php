de5dd9df03d1a904c7ea2a6df1a4e53ba4514907
jbaxax <walterjave92@gmail.com>
2024-09-12 21:44:39 -0500

    first commit

---
+++ app/config.php
@@ -0,0 +1,20 @@
+<?php
+
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','sistemadeventas');
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+try{
+    $pdo = new PDO($servidor,USUARIO,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+    echo "Conexion exitosa";
+
+}catch (PDOException $e){
+    /* print_r($e); */
+
+    echo "Error al conectar a la base de datos";
+}
+
+?>

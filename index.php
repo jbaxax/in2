65ae1c30af9b30d7c7fb1cfb9d23cc55d08e335a
TheCap16 <115736444+TheCap16@users.noreply.github.com>
2024-10-01 01:03:24 -0500

    26

--- index.php
+++ index.php
@@ -1,35 +1,106 @@
 <?php
+// Incluir archivo de configuración
 include('app/config.php');
+
+// Incluir archivo de manejo de sesión
 include('layout/sesion.php');
 
+// Incluir la primera parte del layout (encabezado, menú, etc.)
 include('layout/parte1.php');
 
 ?>
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Bienvenido al SISTEMA DE VENTAS</h1>
-          </div><!-- /.col -->
-         
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-    
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="as">aaa</div>
+  <!-- Encabezado de contenido (encabezado de página) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Bienvenido al SISTEMA DE VENTAS</h1>
+        </div><!-- /.col -->
+
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Contenido principal -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="as">Contenido del Sistema</div>
+      
+      <br><br>
+
+      <div class="row">
+        <div class="col-lg-3 col-6">
+
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>150</h3>
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-shopping-cart"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
+              <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3>44</h3>
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-user-plus"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h3>65</h3>
+              <p>Unique Visitors</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-chart-pie"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
   </div>
-<?php
-include('layout/parte2.php');
-?>
-
-
+  <?php
+  // Incluir la segunda parte del layout (pie de página, scripts, etc.)
+  include('layout/parte2.php');
+  ?>
